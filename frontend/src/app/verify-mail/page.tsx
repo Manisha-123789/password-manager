466603bbd62ca8d1a7a101f675e2e0b89de03594
Manisha Bavniya <bavniya.manisha1997@gmail.com>
2026-03-31 13:17:22 +0530

    build issue fixed

--- frontend/src/app/verify-mail/page.tsx
+++ frontend/src/app/verify-mail/page.tsx
@@ -1,12 +1,13 @@
 import VerifyMail from '@/src/components/auth/VerifyMail';
 import { cookies } from 'next/headers';
 
+
 const Page = async () => {
   const cookieStore = await cookies();
   const token = cookieStore.get('token')?.value;
   return (
     <>
-      <VerifyMail temp={token} />
+      <VerifyMail/>
     </>
   );
 };
