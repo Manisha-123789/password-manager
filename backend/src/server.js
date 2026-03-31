72392d3ee0583a898fdfacb58e052da6633e522b
Manisha Bavniya <bavniya.manisha1997@gmail.com>
2026-03-31 13:59:56 +0530

    frontend url change

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,7 +23,9 @@ app.use(
 );
 app.use(errorHandler);
 app.use(cors({
-  origin : "*"
+  origin : "https://password-manager-7ack1hnt3-manishas-projects-debafcfa.vercel.app/",
+    credentials: true
+
 }));
 app.use(express.json());
 app.use('/user', userRoutes)
